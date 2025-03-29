99825c268aa32124bc22f5e9fc869e35bb7c9912
hamzehhatamleh <hhatamleh2002@gmail.com>
2025-03-29 15:40:46 +0300

    Initial commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,20 @@
+
+package com.example;
+
+public class Main {
+
+    public static int Cubic(int n) {
+        return n*n*n;
+
+    }
+    public static void main(String[] args) {
+
+        System.out.println("HI in Main Class");
+
+
+
+
+
+    }
+}
+}
---
+++ src/test/java/TestClass.java
@@ -0,0 +1,15 @@
+import com.example.Main;
+import static org.junit.Assert.*;
+
+public class TestClass {
+
+
+    public void test1(){
+        assertEquals(27 , Main.Cubic(3));
+
+    }
+}
+
+
+
+
